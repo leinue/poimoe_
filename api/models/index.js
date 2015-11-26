090611d1927c48b89b6585c37f683ccf2b07def9
leinue <597055914@qq.com>
2015-11-26 11:25:16 +0800

    fix overwrite error

--- api/models/index.js
+++ api/models/index.js
@@ -4,13 +4,14 @@ var user = require('./user');
 module.exports = {
   
   mongoose: '',
+  userModel: undefined,
   
   init: function(mongoose) {
     this.mongoose = mongoose;    
   },
   
   User: function() {
-    return user.init(this.mongoose);
+    return this.userModel == undefined ? user.init(this.mongoose) : this.userModel;
   }
 
 };
