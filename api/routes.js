2ace0fc89f5bc6808eb5dfe361e3748e1cc05b8f
leinue <597055914@qq.com>
2015-11-26 18:52:09 +0800

    add route register

--- api/routes.js
+++ api/routes.js
@@ -12,37 +12,8 @@ module.exports = {
     this.mongoose = mongo;
   
     server.get('/log/', ctrl.userCtrl.logUser);
-     
-    server.get('/mail', function(req, res) {
-    	
-    	var smtpTransport = nodemailer.createTransport("SMTP", {
-    		host: 'smtp.qq.com',
-    		secureConnection: true,
-    		port: 465,
-    		auth: {
-    			user: "597055914@qq.com",
-    			pass: 'lanJIA7758'
-    		}
-    	});
 
-    	var mailOptions = {
-		  from: "Fred Foo <597055914@qq.com>", // 发件地址
-		  to: "1054639005@qq.com", // 收件列表
-		  subject: "Hello world", // 标题
-		  html: "<b>thanks a for visiting!</b> 世界，你好！" // html 内容
-		};
-
-		smtpTransport.sendMail(mailOptions, function(err, response) {
-			if(err) {
-				res.send(err);
-			}else {
-				res.send('message senf:' + response.message);
-			}
-			smtpTransport.close();
-		});
-
-
-    });
+    server.get('/user/register/:email/:passsword', ctrl.userCtrl.register);
   
   }
 
