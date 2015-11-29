bdeeca23063ec7e19319628bf30410a2bd661ade
leinue <597055914@qq.com>
2015-11-29 16:24:02 +0800

    add socket server

---
+++ socket/index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.send('welcome to poimoe socket server');
+});
+
+var chat = io.of('/chat');
+var kaku = io.of('/kaku');
+
+chat.on('connection', function(socket){
+	console.log('user connected to chat socket');
+
+	// io.emit('chat message', 'hello fresh');
+
+	socket.on('chat message', function(msg){
+		console.log('msg reveived');
+	    io.emit('chat message', msg);
+	});
+});
+
+kaku.on('connection', function(socket) {
+
+	console.log('user connected to kaku socket');
+
+});
+
+http.listen(9527, function(){
+  console.log('poimoe socket server listening on *:9527');
+});
