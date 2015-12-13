71b5c5d2fdb567a95d2e9dee55a3e7a8b00a1206
leinue <597055914@qq.com>
2015-12-13 10:09:35 +0800

    update index.vue

--- web/poi/src/components/index.vue
+++ web/poi/src/components/index.vue
@@ -1,28 +1,73 @@
 <template>
     
-    <div class="row" style="border-bottom: 1px solid rgb(220, 220, 220);">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="search-area">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="请输入搜索内容">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">搜索</button>
-                    </span>
+    <div>
+        <div class="row" style="border-bottom: 1px solid rgb(220, 220, 220);background-color: #eee;">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="search-area">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="请输入搜索内容">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">搜索</button>
+                        </span>
+                    </div>
+
+                    <div class="tag-list">
+                        <span class="glyphicon glyphicon-tags" style="color: rgb(175, 175, 175);cursor:auto"> 标签：</span>
+                        <span @click="show" class="label label-default">Default</span>
+                        <span class="label label-primary">Primary</span>
+                        <span class="label label-success">Success</span>
+                        <span class="label label-info">Info</span>
+                        <span class="label label-warning">Warning</span>
+                        <span class="label label-danger">Danger</span>
+                        <span class="label label-default">Default</span>
+                        <span class="label label-primary">Primary</span>
+                        <span class="label label-success">Success</span>
+                        <span class="label label-info">Info</span> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row" style="border-bottom: 1px solid rgb(220, 220, 220)">
+            <div class="col-xs-12" style="padding-left:0px;padding-right:0px">
+                
+                <div class="row">
+                    <div class="col-xs-6 col-sm-3" style="border-right: 1px solid rgb(220, 220, 220);padding-left:0px;padding-right:0px;text-align:center">
+                        <div class="profile">
+                            <div class="profile-img"></div>
+                            <div class="profile-photo"></div>
+                            <div class="profile-detail">
+
+                                <div class="col-xs-6 col-sm-4 outer-column">
+                                    <div class="column">
+                                        <p>124</p>
+                                        <span>关注</span>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-4 outer-column">
+                                    <div class="column">
+                                        <p>123</p>
+                                        <span>粉丝</span>
+                                    </div>                                    
+                                </div>
+                                <div class="clearfix visible-xs-block"></div>
+                                <div class="col-xs-6 col-sm-4 outer-column">
+                                    <div class="column none">
+                                        <p>1234</p>
+                                        <span>投稿</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="recommend-user">
+                            
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-9">.col-xs-6 .col-sm-3</div>
+                    <div class="clearfix visible-xs-block"></div>
                 </div>
 
-                <div class="tag-list">
-                    <span class="glyphicon glyphicon-tags" style="color: rgb(175, 175, 175);cursor:auto"> 标签：</span>
-                    <span @click="show" class="label label-default">Default</span>
-                    <span class="label label-primary">Primary</span>
-                    <span class="label label-success">Success</span>
-                    <span class="label label-info">Info</span>
-                    <span class="label label-warning">Warning</span>
-                    <span class="label label-danger">Danger</span>
-                    <span class="label label-default">Default</span>
-                    <span class="label label-primary">Primary</span>
-                    <span class="label label-success">Success</span>
-                    <span class="label label-info">Info</span> 
-                </div>
             </div>
         </div>
     </div>
@@ -55,8 +100,60 @@
     .nav {
         margin: 10px;
     }
+
     .nav li {
         display: inline-block;
         font-size: 25px;
     }
+
+    .profile-img {
+        width: 100%;
+        height: 75px;
+        background-image: url(http://img.t.sinajs.cn/t5/skin/public/profile_cover/015_s.jpg?version=f26b055ccf71bd78);
+    }
+
+    .profile-photo {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        background-image: url(http://tp2.sinaimg.cn/2354504421/180/40057719478/0);
+        background-size: 60px;
+        cursor: pointer;
+        margin: 0 auto;
+        margin-top: -30px;
+    }
+
+    p {
+        font-size: 1.4em!important;
+        margin-bottom: 3px;
+        line-height: .7;
+    }
+
+    .profile-detail {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .profile .outer-column {
+        border-right: 1px solid rgb(220, 220, 220);
+    }
+
+    .profile .outer-column:last-child {
+        border-right: none;
+    }
+
+    .profile .column {
+        margin-bottom: 5px;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    .profile .column:hover {
+        color: rgb(0, 124, 246);
+    }
+
+    .profile .column span {
+        color: rgb(170, 170, 170);
+    }
+
 </style>
